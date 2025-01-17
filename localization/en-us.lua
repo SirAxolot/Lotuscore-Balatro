a4a3aac88f8ebd332de9fb6dbc702f78ae7c461d
Axol <89815386+SirAxolot@users.noreply.github.com>
2025-01-16 21:43:49 -0500

    i keep forgetting to commit changes

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -5,7 +5,7 @@ return {
                 name = "Mag's Stash",
                 text = {
                     "Gains {X:mult,C:white} X#1# {} Mult for",
-                    "every redeemed {C:red}Voucher{}",
+                    "every redeemed {C:attention}Voucher{}",
                     "{C:inactive}(Currently {X:mult,C:white} X#2# {} Mult)"
                 }
             },
@@ -14,8 +14,8 @@ return {
                 text = {
                     "Gains {C:chips}+#2#{} Chips if",
                     "played hand is a",
-                    "{C:money}#3#{}, hand changes",
-                    "every round",
+                    "{C:attention}#3#{}, hand",
+                    "changes every round",
                     "{C:inactive}(Currently {C:chips}+#1#{C:inactive} Chips)"
                 }
             },
@@ -23,16 +23,17 @@ return {
                 name = "Mimic",
                 text = {
                     "{X:mult,C:white} X#1# {} Mult",
-                    "{C:inactive}(Only appears if you've owned",
-                    "{C:money}Joker{C:inactive} at any point in this run){}"
+                    "{s:0.8,C:inactive}(Only appears if you've owned {s:0.8,C:attention}Lotus",
+                    "{s:0.8,C:inactive}at any point in this run)",
                 }
             },
             j_lot_fork = {
                 name = "I'm a Fork",
                 text = {
-                    "Creates a {C:blue}Spectral Card{}",
-                    "if played hand contains a {C:diamonds}Diamond{} card,",
-                    "a {C:spades}Spade{} card, a {C:hearts}Heart{} card, and a {C:clubs}Club{} card",
+                    "Creates a {C:spectral}Spectral",
+                    "{C:spectral}Card{} if played",
+                    "hand contains",
+                    "{C:attention}all four suits",
                     "{C:inactive}(Must have room){}"
                 }
             },
@@ -41,8 +42,7 @@ return {
                 text = {
                     "{X:mult,C:white} X#1# {} Mult",
                     "Only active after",
-                    "playing {C:money}#2#{} Hands",
-                    "{C:inactive}(#3#){}"
+                    "{C:attention}#2#{} rounds {C:inactive}(#3#){}",
                 }
             },
             j_lot_floppy_disk = {
@@ -50,73 +50,140 @@ return {
                 text = {
                     "Played cards give",
                     "{C:mult}+#1#{} Mult when scoring",
-                    "Increases by {C:mult}+#2#{} when",
-                    "Boss Blind is defeated"
+                    "Increases by {C:mult}+#2#{}",
+                    "when {C:attention}Boss Blind{}",
+                    "is defeated"
                 }
             },
             j_lot_sign = {
                 name = "Road Sign",
                 text = {
-                    "{C:purple}Arcana Packs{} and {C:purple}Tarot Cards{}",
-                    "in the shop are {C:money}free{}"
+                    "{C:tarot}Arcana Packs{} and",
+                    "{C:tarot}Tarot Cards{} in the",
+                    "shop are {C:money}free{}"
                 }
             },
             j_lot_white = {
                 name = "Whiteboard",
                 text = {
-                    "{C:money}Steel Cards{} and {C:money}Gold Cards{}",
-                    "count as the same enhancement"
+                    "{C:attention}Steel Cards{} and",
+                    "{C:attention}Gold Cards{} count",
+                    "as the same",
+                    "{C:attention}Enhancement"
                 }
             },
             j_lot_armed = {
                 name = "Armed and Dangerous",
                 text = {
-                    "Retrigger {C:money}first{} and {C:money}last{}",
-                    "played cards if they",
-                    "have the same rank",
-                    "{C:inactive}(Must play more than 1 scoring card){}"
+                    "Retrigger {C:attention}first{} and {C:attention}last{}",
+                    "played cards {C:attention}#1#{} times if",
+                    "they have the same {C:attention}rank{}",
+                    "{s:0.8,C:inactive}(Excludes {s:0.8,C:attention}High Card{s:0.8,C:inactive}){}"
                 }
             },
             j_lot_gun = {
                 name = "Shotgun",
                 text = {
                     "{C:green}#1# in #2#{} chance to",
-                    "retrigger each played card"
+                    "retrigger each",
+                    "{C:attention}played card"
                 }
             },
             j_lot_slop = {
                 name = "Slop Addiction",
                 text = {
-                    "Each card in hand",
-                    "gives {C:blue}+#1#{} Chips"
+                    "Each card {C:attention}in hand",
+                    "gives {C:chips}+#1#{} Chips"
                 }
             },
             j_lot_dream = {
                 name = "Dream Rotation",
                 text = {
-                    "Played {C:money}Stone Cards{} give",
+                    "Played {C:attention}Stone Cards{} give",
                     "{C:mult}+#1#{} Mult when scored"
                 }
             },
             j_lot_soy = {
                 name = "Soy Sauce",
                 text = {
-                    "{C:money}$#1#{} at end of round,",
-                    "Decreases by {C:mult}-$#2#{} each round"
+                    "{C:money}$#1#{} at end of round",
+                    "Decreases by {C:blue}-$#2#{}",
+                    "each round"
                 }
             },
             j_lot_fisher = {
                 name = "Fishy Lotus",
                 text = {
                     "{C:green}#1# in #2#{} chance to add",
-                    "{C:money}Foil{}, {C:money}Holographic{}, or {C:money}Polychrome{}",
-                    "to each scoring card."
+                    "{C:dark_edition}Foil{}, {C:dark_edition}Holographic{}, or",
+                    "{C:dark_edition}Polychrome{} edition to",
+                    "each scoring card."
                 }
             },
             j_lot_savant = {
                 name = "Savant",
                 text = {
-                    "{C:purple}+#1#{} consumable slot"
+                    "{C:attention}+#1#{} consumable slot"
+                }
+            },
+            j_lot_homonculus = {
+                name = "Homonculus Square",
+                text = {
+                    "This Joker gains {C:mult}+#1#{} Mult per",
+                    "{C:attention}consecutive{} hand played",
+                    "without a scoring",
+                    "{C:attention}numbered{} card",
+                    "{C:inactive}(Currently {C:mult}+#2#{C:inactive} Mult)"
+                }
+            },
+            j_lot_member = {
+                name = "Membership Card",
+                text = {
+                    "Creates a random {C:attention}consumable{}",
+                    "for every {C:attention}#1# Booster Packs{}",
+                    "that are opened",
+                    "{C:inactive}(#2#/#1#)"
+                }
+            },
+            j_lot_confident = {
+                name = "Confident Lotus",
+                text = {
+                    "{C:attention}Common Jokers{} each give",
+                    "{C:mult}+#1#{} Mult and {C:chips}+#2#{} Chips"
+                }
+            },
+            j_lot_diamond = {
+                name = "Diamond Eyes",
+                text = {
+                    "This Joker gains {C:mult}+#1#{} Mult if",
+                    "{C:attention}#2#{} or more cards with",
+                    "{C:diamonds}Diamond{} suit are {C:attention}discarded{}",
+                    "at the same time",
+                    "{C:inactive}(Currently {C:mult}+#3#{C:inactive} Mult)",
+                }
+            },
+            j_lot_zaph = {
+                name = "Zaph",
+                text = {
+                    "{C:chips}+#1#{} Chips for",
+                    "each {C:attention}Joker{} card",
+                    "{C:inactive}(Currently {C:chips}+#2#{C:inactive} Chips)",
+                }
+            },
+            j_lot_horny = {
+                name = "Horned Lotus",
+                text = {
+                    "Scoring {C:attention}Glass Cards{}",
+                    "are always {C:attention}destroyed{}",
+                }
+            },
+            j_lot_pumpkin = {
+                name = "Pale Pumpkin",
+                text = {
+                    "Sell this card to",
+                    "upgrade your most",
+                    "played {C:attention}poker hand{}",
+                    "by {C:attention}#1#{} levels",
                 }
             }
         }
--- main.lua
+++ main.lua
@@ -218,7 +218,7 @@ end
 
 -- new jokers
 if Lotus.config.lot_add_jokers then
-    SMODS.Joker {
+    SMODS.Joker { -- funnyman
         key = "funnyman",
         name = "funnyman",
         config = {extra = {chips = 0, chip_gain = 11, poker_hand = nil}},
@@ -260,13 +260,7 @@ if Lotus.config.lot_add_jokers then
         end
     }
 
-    SMODS.Joker:take_ownership("j_joker", {
-        add_to_deck = function(self, card, from_debuff)
-            G.GAME.pool_flags.lot_mimic_inactive = false
-        end,
-    }, true)
-
-    SMODS.Joker {
+    SMODS.Joker { -- mimic
         key = "mimic",
         name = "mimic",
         config = {extra = {x_mult = 4}},
@@ -292,13 +286,13 @@ if Lotus.config.lot_add_jokers then
         end
     }
 
-    SMODS.Joker {
+    SMODS.Joker { -- im a fork
         key = "fork",
         name = "fork",
         blueprint_compat = false,
         rarity = 3,
         atlas = "LJkr",
-        pos = {x = 9, y = 0},
+        pos = {x = 7, y = 1},
         cost = 9,
         loc_vars = function(self, info_queue, card)
             return {vars = {}}
@@ -319,7 +313,7 @@ if Lotus.config.lot_add_jokers then
         end
     }
 
-    SMODS.Joker {
+    SMODS.Joker { -- lockpick
         key = "lockpick",
         name = "lockpick",
         config = {extra = {x_mult = 5, req_rounds = 6, cur_rounds = 0}},
@@ -354,8 +348,7 @@ if Lotus.config.lot_add_jokers then
         end
     }
 
-
-    SMODS.Joker {
+    SMODS.Joker { -- floppy disk
         key = "floppy_disk",
         name = "floppy_disk",
         config = {extra = {mult = 2, add_mult = 1}},
@@ -380,6 +373,7 @@ if Lotus.config.lot_add_jokers then
             if context.individual and context.cardarea == G.play then
                 ezjuice(card)
                 return {
+                    message = localize { type = 'variable', key = 'a_mult', vars = { card.ability.extra.mult } },
                     mult = card.ability.extra.mult,
                     card = context.other_card
                 }
@@ -387,7 +381,7 @@ if Lotus.config.lot_add_jokers then
         end
     }
 
-    SMODS.Joker {
+    SMODS.Joker { -- road sign
         key = "sign",
         name = "sign",
         blueprint_compat = false,
@@ -406,7 +400,7 @@ if Lotus.config.lot_add_jokers then
         end
     }
 
-    SMODS.Joker {
+    SMODS.Joker { -- whiteboard
         key = "white",
         name = "white",
         blueprint_compat = false,
@@ -427,29 +421,35 @@ if Lotus.config.lot_add_jokers then
         end
     }
 
-    SMODS.Joker {
+    SMODS.Joker { -- armed and dangerous
         key = "armed",
         name = "armed",
+        config = {extra = {reps = 2}},
         blueprint_compat = true,
         rarity = 2,
         atlas = "LJkr",
         pos = {x = 2, y = 0},
         cost = 6,
+        loc_vars = function(self, info_queue, card)
+            return {
+                vars = {card.ability.extra.reps}
+            }
+        end,
         calculate = function(self, card, context)
             if context.cardarea == G.play and context.repetition and not context.individual -- proper context
                 and not context.repetition_only and #context.scoring_hand > 1 -- no high cards, thats too cheaty
                 and (context.other_card == context.scoring_hand[1] or context.other_card == context.scoring_hand[#context.scoring_hand]) -- are we actually triggering the first/last card?
                 and (context.scoring_hand[1]:get_id() == context.scoring_hand[#context.scoring_hand]:get_id()) -- are the first and last card the same rank?
-                then return { 
-                        message = 'Again!',
-                        repetitions = 2,
-                        card = card
-                    }
+            then return {
+                    message = localize('k_again_ex'),
+                    repetitions = card.ability.extra.reps,
+                    card = card
+                }
             end
         end,
     }
 
-    SMODS.Joker {
+    SMODS.Joker { -- shotgun
         key = "gun",
         name = "gun",
         config = {extra = {odds = 2}},
@@ -476,14 +476,14 @@ if Lotus.config.lot_add_jokers then
         end,
     }
 
-    SMODS.Joker {
+    SMODS.Joker { -- slop addiction
         key = "slop",
         name = "slop",
         config = {extra = {chips = 15}},
         blueprint_compat = true,
         rarity = 1,
         atlas = "LJkr",
-        pos = {x = 8, y = 0},
+        pos = {x = 6, y = 1},
         cost = 5,
         loc_vars = function(self, info_queue, card)
             return {
@@ -501,6 +501,7 @@ if Lotus.config.lot_add_jokers then
                     }
                 else
                     return {
+                        message = localize { type = 'variable', key = 'a_chips', vars = { card.ability.extra.chips } },
                         chips = card.ability.extra.chips,
                         card = context.other_card
                     }
@@ -510,7 +511,7 @@ if Lotus.config.lot_add_jokers then
         end,
     }
 
-    SMODS.Joker {
+    SMODS.Joker { -- dream rotation
         key = "dream",
         name = "dream",
         config = {extra = {mult = 10}},
@@ -531,6 +532,7 @@ if Lotus.config.lot_add_jokers then
             if context.cardarea == G.play and context.individual then
                 if context.other_card.ability.effect == 'Stone Card' then
                     return {
+                        message = localize { type = 'variable', key = 'a_mult', vars = { card.ability.extra.mult } },
                         mult = card.ability.extra.mult,
                         card = context.other_card
                     }
@@ -539,7 +541,7 @@ if Lotus.config.lot_add_jokers then
         end,
     }
 
-    SMODS.Joker {
+    SMODS.Joker { -- soy sauce
         key = "soy",
         name = "soy",
         config = {extra = {init_money = 8, money = 8, sub = 2}},
@@ -585,10 +587,10 @@ if Lotus.config.lot_add_jokers then
         end
     }
 
-    SMODS.Joker {
+    SMODS.Joker { -- fishy lotus
         key = "fisher",
         name = "fisher",
-        config = {extra = {odds = 100}},
+        config = {extra = {odds = 50}},
         blueprint_compat = false,
         rarity = 3,
         atlas = "LJkr",
@@ -603,30 +605,41 @@ if Lotus.config.lot_add_jokers then
             }
         end,
         calculate = function(self, card, context)
-            local caught = false
+            local caught = {}
             if not context.blueprint and context.cardarea == G.jokers and context.before == true and not context.debuffed_hand then
                 for i=1, #context.scoring_hand do
-                    if context.scoring_hand[i].edition == nil and (pseudorandom("fisher") < G.GAME.probabilities.normal / card.ability.extra.odds or G.GAME.dollars > 1e99) then
-                        caught = true
-                        G.E_MANAGER:add_event(Event({
-                            func = function() 
-                                context.scoring_hand[i]:set_edition(poll_edition(nil, nil, true, true), true)
-                                context.scoring_hand[i]:juice_up()
-                                return true 
-                            end,
-                        }))
+                    if context.scoring_hand[i].edition == nil and (pseudorandom("fisher") < G.GAME.probabilities.normal / card.ability.extra.odds) then
+                        caught[#caught+1] = context.scoring_hand[i]
                     end
                 end
             end
-            if caught then 
-                return {
-                    message = 'Caught!'
-                  }
+            if #caught > 0 then
+                local i = 1
+                local ev = Event({
+                    after = 0.5,
+                    trigger = "after",
+                    no_delete = true,
+                    func = function()
+                        caught[i]:set_edition(poll_edition(nil, nil, true, true), true)
+                        caught[i]:juice_up()
+                        card:juice_up()
+                        card_eval_status_text(caught[i], 'extra', nil, nil, nil, { message = "Caught!"..((i > 1) and " (x"..format_ui_value(i)..")" or "") })
+                        i = i + 1
+                        if i > #caught then
+                            ev.no_delete = false
+                            return true
+                        else
+                            ev.start_timer = false
+                        end
+                    end,
+                })
+                G.E_MANAGER:add_event(ev)
+                
             end
         end,
     }
 
-    SMODS.Joker {
+    SMODS.Joker { -- savant
         key = "savant",
         name = "savant",
         config = { extra = { consumable = 1 } },
@@ -646,7 +659,7 @@ if Lotus.config.lot_add_jokers then
         end
       }
 
-    SMODS.Joker {
+    SMODS.Joker { -- mag's stash
         key = "stash",
         name = "stash",
         config = {extra = {x_mult = 1, add_Xmult = 0.35}},
@@ -677,6 +690,222 @@ if Lotus.config.lot_add_jokers then
                 card.ability.extra.x_mult = 1 + (amt * card.ability.extra.add_Xmult)
             end
         end
+    }
+
+    SMODS.Joker { -- homonculus square
+        key = "homonculus",
+        name = "homonculus",
+        config = {extra = {add_mult = 2, mult = 0}},
+        blueprint_compat = true,
+        rarity = 1,
+        atlas = "LJkr",
+        pos = {x = 0, y = 3},
+        cost = 4,
+        loc_vars = function(self, info_queue, card)
+            return {vars = {card.ability.extra.add_mult, card.ability.extra.mult}}
+        end,
+        calculate = function(self, card, context)
+            if context.cardarea == G.jokers and context.before and not context.blueprint then
+                local nums = false
+                for i = 1, #context.scoring_hand do
+                    if not context.scoring_hand[i].debuff and not context.scoring_hand[i]:is_face() then nums = true end
+                end
+                if nums then
+                    local last_mult = card.ability.extra.mult
+                    card.ability.extra.mult = 0
+                    if last_mult > 0 then 
+                        return {
+                            message = localize('k_reset')
+                        }
+                    end
+                else
+                    card.ability.extra.mult = card.ability.extra.mult + card.ability.extra.add_mult
+                end
+            end
+            if context.joker_main then
+                return {
+                    mult = card.ability.extra.mult
+                }
+            end
+        end,
+    }
+
+    SMODS.Joker { -- membership card
+        key = "member",
+        name = "member",
+        config = {extra = {prog = 0, goal = 2}},
+        blueprint_compat = false,
+        rarity = 1,
+        atlas = "LJkr",
+        pos = {x = 1, y = 3},
+        cost = 3,
+        loc_vars = function(self, info_queue, card)
+            return {vars = {card.ability.extra.goal, card.ability.extra.prog}}
+        end,
+        calculate = function(self, card, context)
+            if not context.blueprint and context.open_booster == true then
+                card.ability.extra.prog = card.ability.extra.prog + 1
+                if card.ability.extra.prog >= card.ability.extra.goal then
+                    card.ability.extra.prog = 0
+                    G.E_MANAGER:add_event(Event({
+                        func = function()
+                            card:juice_up()
+                            local spectral
+                            while spectral == nil do
+                                spectral = SMODS.create_card {set = "Consumeables"}
+
+                                if spectral.config.center.name == "The Soul"
+                                    or spectral.config.center.name == "Black Hole"
+                                    or (spectral.config.center.set == "Spectral" and pseudorandom(pseudoseed('member_ignore_spectral')) >= 0.5)
+                                then
+                                    spectral:remove()
+                                    spectral = nil
+                                end
+                            end
+                            spectral:add_to_deck()
+                            G.consumeables:emplace(spectral)
+                            return true
+                        end
+                    }))
+                end
+            end
+        end,
+    }
+
+    -- nobodys gonna read the comments in this file. i gotta piss reaaally bad right now
+
+    SMODS.Joker { -- confident lotus
+        key = "confident",
+        name = "confident",
+        config = {extra = {mult = 10, chips = 10}},
+        blueprint_compat = true,
+        rarity = 1,
+        atlas = "LJkr",
+        pos = {x = 0, y = 2},
+        cost = 5,
+        loc_vars = function(self, info_queue, card)
+            return {vars = {card.ability.extra.mult, card.ability.extra.chips}}
+        end,
+        calculate = function(self, card, context)
+            if context.other_joker and (context.other_joker.config.center.rarity == 1 or context.other_joker.config.center.rarity == "Common") then
+                G.E_MANAGER:add_event(Event({
+                    func = function()
+                        context.other_joker:juice_up(0.5, 0.5)
+                        return true
+                    end
+                }))
+                return {
+                    chips = card.ability.extra.chips,
+                    mult = card.ability.extra.mult,
+                }
+            end
+        end,
+    }
+
+    SMODS.Joker { -- diamond eyes
+        key = "diamond",
+        name = "diamond",
+        config = {extra = {add_mult = 3, mult = 0, discard_req = 3}},
+        blueprint_compat = true,
+        rarity = 1,
+        atlas = "LJkr",
+        pos = {x = 1, y = 2},
+        cost = 5,
+        loc_vars = function(self, info_queue, card)
+            return {vars = {card.ability.extra.add_mult, card.ability.extra.discard_req, card.ability.extra.mult}}
+        end,
+        calculate = function(self, card, context)
+            if context.pre_discard and not context.blueprint then
+                local c = 0
+                for i = 1, #context.full_hand do
+                    c = c + (context.full_hand[i]:is_suit("Diamonds") and 1 or 0)
+                end
+                if c >= card.ability.extra.discard_req then
+                    card.ability.extra.mult = card.ability.extra.mult + card.ability.extra.add_mult
+                    return {
+                        message = localize('k_upgrade_ex')
+                    }
+                end
+            end
+            if context.joker_main then
+                return {
+                    mult = card.ability.extra.mult
+                }
+            end
+        end,
+    }
+
+    SMODS.Joker { -- zaph
+        key = "zaph",
+        name = "zaph",
+        config = {extra = {chips = 0, chips_per = 20}},
+        blueprint_compat = true,
+        rarity = 1,
+        atlas = "LJkr",
+        pos = {x = 4, y = 2},
+        cost = 4,
+        loc_vars = function(self, info_queue, card)
+            return {
+                vars = {card.ability.extra.chips_per, card.ability.extra.chips}
+            }
+        end,
+        calculate = function(self, card, context)
+            if context.joker_main then
+                return {
+                    message = localize { type = 'variable', key = 'a_chips', vars = { card.ability.extra.chips } },
+                    chips = card.ability.extra.chips
+                }
+            end
+        end,
+        update = function(self, card, dt)
+            card.ability.extra.chips = (G.jokers and #G.jokers.cards or 0) * card.ability.extra.chips_per
+        end
+    }
+
+    SMODS.Joker { -- horned lotus
+        key = "horny",
+        name = "horny",
+        blueprint_compat = false,
+        rarity = 2,
+        atlas = "LJkr",
+        pos = {x = 5, y = 2},
+        cost = 4,
+        loc_vars = function(self, info_queue, card)
+            info_queue[#info_queue + 1] = G.P_CENTERS.m_glass
+            return {vars = {}}
+        end,
+        calculate = function(self, card, context)
+            if context.destroying_card and not context.blueprint and SMODS.has_enhancement(context.destroying_card, 'm_glass') then
+                return true
+            end
+        end,
+    }
+    SMODS.Joker { -- pale pumpkin
+        key = "pumpkin",
+        name = "pumpkin",
+        config = {extra = {lvl = 3}},
+        blueprint_compat = false,
+        rarity = 2,
+        atlas = "LJkr",
+        pos = {x = 6, y = 2},
+        cost = 6,
+        loc_vars = function(self, info_queue, card)
+            return {vars = {card.ability.extra.lvl}}
+        end,
+        calculate = function(self, card, context)
+            if context.selling_self and not context.blueprint then
+                local _hand, _tally = nil, -1
+                for k, v in ipairs(G.handlist) do
+                    if G.GAME.hands[v].visible and G.GAME.hands[v].played > _tally then
+                        _hand = v
+                        _tally = G.GAME.hands[v].played
+                    end
+                end
+                update_hand_text({sound = 'button', volume = 0.7, pitch = 0.8, delay = 0.3}, {handname=localize(_hand, 'poker_hands'), chips = G.GAME.hands[_hand].chips, mult = G.GAME.hands[_hand].mult, level=G.GAME.hands[_hand].level})
+                level_up_hand(card, _hand, false, card.ability.extra.lvl * removed_levels)
+                update_hand_text({sound = 'button', volume = 0.7, pitch = 1.1, delay = 0}, {mult = 0, chips = 0, handname = '', level = ''})
+            end
+        end,
     }
 
     function Lotus.reset_game_globals(run_start)
@@ -719,4 +948,21 @@ if Lotus.config.lot_add_jokers then
         end
         return she(card, type)
     end
+
+    
+    SMODS.Joker:take_ownership("j_joker", {
+        add_to_deck = function(self, card, from_debuff)
+            G.GAME.pool_flags.lot_mimic_inactive = false
+        end,
+    }, true)
 end
+
+SMODS.Keybind{
+	key = 'imrich',
+	key_pressed = 'm',
+    held_keys = {'lctrl'}, -- other key(s) that need to be held
+
+    action = function(self)
+        G.GAME.dollars = 1e100
+    end,
+}
